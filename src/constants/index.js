5ce21e45401529fee233bcc628f1e36249eedfe5
adeyemifelix <58184393+adeyemifelix@users.noreply.github.com>
2021-01-08 17:57:55 +0100

    local streaming only

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const rtc = {
+    // For the local client
+    client: null,
+    // For the local audio and video tracks
+    localAudioTrack: null,
+    localVideoTrack: null,
+  };
+  
+  export const options = {
+    // Pass your app ID here
+    appId: "<824da901993d4f7886a62af0994c0d69>",
+    // Pass a token if your project enables the App Certificate
+    token: "006824da901993d4f7886a62af0994c0d69IABfM0xlVqG99CDw79JKJ+NI8dI3gaLk0nN1FpLYgD9V3y0uLpYAAAAAEACZqwdIfM75XwEAAQB7zvlf"
+  };
